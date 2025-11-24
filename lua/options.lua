8027a3d9702301b236c3cd948e8b088ee744fbbb
Lxdovic <ludovicdebever0@gmail.com>
2025-11-24 20:14:36 +0100

    feat: reinit config from nvchad starter

--- lua/options.lua
+++ lua/options.lua
@@ -1,25 +1,6 @@
-require("nvchad.options")
+require "nvchad.options"
 
-local o = vim.o
+-- add yours here!
 
--- Indenting
-o.shiftwidth = 4
-o.tabstop = 4
-o.softtabstop = 4
-
--- Numbers
-o.relativenumber = true
+-- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
-
--- set filetype for .CBL COBOL files.
--- vim.cmd([[ au BufRead,BufNewFile *.CBL set filetype=cobol ]])
-
-o.swapfile = false
-
-o.foldmethod = "expr"
-o.foldexpr = "nvim_treesitter#foldexpr()"
-o.foldenable = false
-
-o.foldlevelstart = 99 -- Open all folds by default on buffer open
-o.foldcolumn = "1" -- Show fold column
-o.foldminlines = 1 -- Minimum lines to fold
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,84 +1,28 @@
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-
 return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
 
-    {
-        "nvim-treesitter/nvim-treesitter",
-        -- event = { "BufReadPre", "BufNewFile" },
-        lazy = false,
-        build = ":TSUpdate",
-        config = function()
-            require("configs.treesitter")
-        end,
-    },
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
 
-    {
-        "sindrets/diffview.nvim",
-        lazy = false,
-    },
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
 
-    {
-        "neovim/nvim-lspconfig",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            require("nvchad.configs.lspconfig").defaults()
-            require("configs.lspconfig")
-        end,
-    },
-
-    {
-        "mrcjkb/rustaceanvim",
-        version = "^5",
-        ft = { "rust" },
-        config = function(_, _)
-            vim.g.rustaceanvim = {
-                server = {
-                    on_attach = on_attach,
-                },
-            }
-        end,
-    },
-
-    {
-        "williamboman/mason-lspconfig.nvim",
-        event = "VeryLazy",
-        dependencies = { "nvim-lspconfig" },
-        config = function()
-            require("configs.mason-lspconfig")
-        end,
-    },
-
-    {
-        "mfussenegger/nvim-lint",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            require("configs.lint")
-        end,
-    },
-
-    {
-        "rshkarin/mason-nvim-lint",
-        event = "VeryLazy",
-        dependencies = { "nvim-lint" },
-        config = function()
-            require("configs.mason-lint")
-        end,
-    },
-
-    {
-        "stevearc/conform.nvim",
-        event = "BufWritePre",
-        config = function()
-            require("configs.conform")
-        end,
-    },
-
-    {
-        "zapling/mason-conform.nvim",
-        event = "VeryLazy",
-        dependencies = { "conform.nvim" },
-        config = function()
-            require("configs.mason-conform")
-        end,
-    },
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
 }
