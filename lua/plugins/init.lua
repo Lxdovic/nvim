47e10d9c312cee8939c1b237143e2fad45064ba2
Ludovic <ludovicdebever0@gmail.com>
2025-11-25 08:36:50 +0100

    fix(diffview): fix diffview loading + remove noice

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = "BufWritePre", -- uncomment for format on save
+    event = "BufWritePre",
     opts = require "configs.conform",
   },
 
@@ -23,7 +23,8 @@ return {
 
   {
     "sindrets/diffview.nvim",
-    dependencies = "nvim-lua/plenary.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
     -- TODO: Add mappings for diffview commands
   },
 
@@ -74,34 +75,4 @@ return {
     end,
   },
 
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      -- TODO
-    },
-
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
-    },
-    config = function()
-      -- Example mapping to toggle outline
-
-      require("noice").setup {
-        presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
-          command_palette = true, -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false, -- add a border to hover docs and signature help
-        },
-        -- Your setup opts here (leave empty to use defaults)
-      }
-    end,
-  },
-}
+  }
